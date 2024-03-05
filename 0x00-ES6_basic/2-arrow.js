69f2d1ed6b8b56e38a689c738afba57151b22d04
salathiel wekesa <wekesa9235@gmail.com>
2024-03-05 10:43:58 +0300

    Standard fx to ES6

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -1,17 +1,8 @@
-  class getNeighborhoodsList{
-
-    constructor(){
-
-      this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
-
-    };
-
-    addNeighborhood = (newNeighborhood)=>{
+export default function getNeighborhoodsList() {
+    this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+  
+    this.addNeighborhood = (newNeighborhood) => {
       this.sanFranciscoNeighborhoods.push(newNeighborhood);
       return this.sanFranciscoNeighborhoods;
     };
-
   }
-
-  export default getNeighborhoodsList;
-  
